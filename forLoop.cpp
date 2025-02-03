a57296b9393d5ebb4c873660ce05d72fb371197e
Im_upset <pattersonroge12@gmail.com>
2025-02-03 21:24:51 +0300

    Completed the code

--- forLoop.cpp
+++ forLoop.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 
+using namespace std;
+
 void pointForLoop(){
 	// Declearations
-	int arr = {1,2,3,4,5,6,7,8,9};
-	int *i = nullptr;
-	int *j = nullptr;
+	int arr[] = {1,2,3,4,5,6,7,8,9};
+	int size = 9;
+	int i, j, t;
 
-	for(){
-		// code goes here
-	}
-
+	for(i = 0; i < size; i++){
+		for(j = i + 1; j < size; j++){
+			if(*(arr + j) < *(arr + i)){
+				t = *(arr + i);
+				*(arr + i) = *(arr + j);
+				*(arr + j) = t; 		
+			}
+		}
+		cout << arr[i] << "\n";
+	}	
 }
 
 
