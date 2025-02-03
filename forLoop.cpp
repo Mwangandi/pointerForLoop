00f56c5e47320247e851264546503632a7ffecb0
Im_upset <pattersonroge12@gmail.com>
2025-02-03 09:22:31 +0300

    Added declarations and for loop

---
+++ forLoop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void pointForLoop(){
+	// Declearations
+	int arr = {1,2,3,4,5,6,7,8,9};
+	int *i = nullptr;
+	int *j = nullptr;
+
+	for(){
+		// code goes here
+	}
+
+}
+
+
+
+int main(){
+	// Function call
+	pointForLoop();
+
+}
